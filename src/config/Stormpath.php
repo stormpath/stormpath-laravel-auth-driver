064bd1ed973366792303cea9edf62da4edb672f5
Brian Retterer <brian@stormpath.com>
2015-09-18 17:05:00 -0400

    Add License and remove env() from config until we get it working correctly with code coverage

--- src/config/Stormpath.php
+++ src/config/Stormpath.php
@@ -2,10 +2,10 @@
 
 return [
 
-    "id" => env("STORMPATH_ID", "YouStormpathId"),
+    "id" => "YouStormpathId",
 
-    "secret" => env("STORMPATH_SECRET", "YourStormpathSecret"),
+    "secret" => "YourStormpathSecret",
 
-    "application" => env("STORMPATH_APPLICATION", "YourStormpathApplicationId")
+    "application" => "YourStormpathApplicationId"
 
 ];
